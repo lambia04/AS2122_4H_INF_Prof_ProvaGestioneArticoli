de851eaca94273c2458ad7be913ea9958503beab
Lambiase <lambiasemassimo2004@gmail.com>
2022-03-28 08:22:12 +0200

    Aggiunta attributi e property alla classe Articolo

--- AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -9,15 +9,24 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
     class Articolo
     {
         // TODO: (8) aggiungere attributi privati di Articolo
-        // ...
+        private string descrizione;
+        private string unitaMisura;
+        private double prezzo;
+        private int codice;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
-        // ...
+        int Codice { get; set; }
+        double Prezzo { get; set; }
+        string Descrizione { get; set; }
+        string UnitaMisura { get; set; }
 
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
-            // ...
+            Codice = codice;
+            Descrizione = descrizione;
+            UnitaMisura = unitaMisura;
+            Prezzo = prezzo;
         }
 
         public string Visualizzati()
